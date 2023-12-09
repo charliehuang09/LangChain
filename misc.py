869a7ac5c499d80d8ba9b277fefa891182065f47
charliehuang09 <41605449+charliehuang09@users.noreply.github.com>
2023-12-09 13:44:28 -0800

    _

--- misc.py
+++ misc.py
@@ -1,4 +1,5 @@
 import os
+from tqdm import trange
 def getFiles(path):
     paths = []
     for filename in os.listdir(path):
@@ -8,3 +9,39 @@ def getFiles(path):
             paths = paths + getFiles(os.path.join(path, filename))
             # paths.append(getFiles(os.path.join(path, filename)))
     return paths
+
+def template(context, prompt):
+    template = f"""Answer the question with context: <context>{context}<context> Question: <question>{prompt}<question>
+    """
+    return template
+
+def GPT4(prompt, client):
+    response = client.chat.completions.create(
+        model="gpt-4-1106-preview",
+        temperature=0,
+        messages=[
+            # {
+            #     "role": "system",
+            #     "content": ""
+            # },
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
+    )
+    return response
+
+def removeNull(documents):
+    for i in trange(len(documents)):
+        documents[i].page_content = documents[i].page_content.replace("\x00", "\uFFFD")
+    return documents
+
+def printContext(context):
+    for i in range(len(context)):
+        print(f"##CONTEXT## {i+1}: {context[i].page_content}")
+
+def removeDots(documents):
+    for i in trange(len(documents)):
+        documents[i].page_content = documents[i].page_content.replace("..................", ".")
+    return documents
--- pull.py
+++ pull.py
@@ -1,32 +1,38 @@
+from calendar import prmonth
 from cgitb import text
-from pydoc import doc
+from pydoc import cli, doc
+from urllib import response
 from langchain.document_loaders import DirectoryLoader, TextLoader, PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores.pgvector import PGVector
 from langchain.docstore.document import Document
-
+from openai import OpenAI
 from dotenv import load_dotenv
 from key import *
 from misc import *
 from tqdm import tqdm
 import os
 
-paths = getFiles(PATH)
-text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap=20)
+client = OpenAI(
+    api_key=OPENAI_KEY,
+)
+
 embeddings = OpenAIEmbeddings()
 store = PGVector(
     collection_name=COLLECTION_NAME,
     connection_string=CONNECTION_STRING,
     embedding_function=OpenAIEmbeddings()
 )
-loader = DirectoryLoader(PATH, 
-                         show_progress=True, 
-                         use_multithreading=True,
-                         glob="**/*.[pr][ds][ft]"
-                         )
-documents = loader.load()
-documents = removeNull(documents)
-documents = removeDots(documents)
-documents = text_splitter.split_documents(documents)
-store.add_documents(documents)
+
+prompt = "What are some ways to score in a FRC game?"
+context = store.similarity_search_with_score(prompt)[0][0].page_content
+prompt = template(context, prompt)
+
+print(prompt)
+
+response = GPT4(prompt, client)
+
+print(response.choices[0].message.content)
+
+
